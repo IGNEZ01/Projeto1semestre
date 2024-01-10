bd60ec479397b713d8f763e90b268eafdf545e48
TitoLagartoCorreia <150141807+TitoLagartoCorreia@users.noreply.github.com>
2024-01-10 14:31:41 +0000

    V0.0.2

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lib.c"
 
-int main(){
-
-    
+int main()
+{
+    //chama o menu inicial da do programa
+    mainMenu();
 }
